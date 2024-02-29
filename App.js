5987728ce85f435b36985c33585dbb1fa67f77e7
Johnny <johnkcli@hotmail.com>
2024-02-29 14:21:37 +0000

    anon sign in with firebase works

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { onAuthStateChanged, signInAnonymously } from "firebase/auth";
+import { auth } from "./firebaseConfig.js";
 
 export default function App() {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      console.log(uid);
+    } else {
+      signInAnonymously(auth);
+    }
+  });
+
   return (
     <View style={styles.container}>
       <Text>Hello, World!</Text>
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
