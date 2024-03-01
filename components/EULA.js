8ecf4c6411f9c3252f1be2b1412e93b945b0861e
Johnny <johnkcli@hotmail.com>
2024-03-01 17:34:53 +0000

    eula message working

--- components/EULA.js
+++ components/EULA.js
@@ -1,38 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Pressable, Modal } from "react-native";
+import { Modal, StyleSheet, Text, Pressable, View } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
-import { onAuthStateChanged, signInAnonymously } from "firebase/auth";
 import { auth } from "../firebaseConfig.js";
 import { agreeEula } from "../redux/eula.js";
 
-export default function Home() {
+const EULA = () => {
   const eula = useSelector((state) => state.eula.agreed);
-  const backendUrl = process.env.EXPO_PUBLIC_BACKEND_URL;
   const dispatch = useDispatch();
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      user.getIdToken(true).then(async (token) => {
-        const response = await fetch(`${backendUrl}/eula-agreed/`, {
-          method: "GET",
-          headers: new Headers({
-            Authorization: token,
-          }),
-        });
-        const ResponseJson = await response.json();
-        dispatch(agreeEula(ResponseJson.agreed_to_eula));
-      });
-    } else {
-      signInAnonymously(auth);
-    }
-  });
   return (
-    <View style={styles.container}>
+    <View style={styles.centeredView}>
       <Modal animationType="slide" transparent={true} visible={!eula}>
         <View style={styles.centeredView}>
           <View style={styles.modalView}>
-            <Text style={styles.modalText}>
-              You must agree to the End User License Agreement
-            </Text>
+            <Text style={styles.modalText}>EULA</Text>
             <Pressable
               style={[styles.button, styles.buttonClose]}
               onPress={() => {
@@ -57,19 +36,11 @@ export default function Home() {
           </View>
         </View>
       </Modal>
-      <Text>Hello, World!</Text>
-      <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "green",
-    alignItems: "center",
-    justifyContent: "center",
-  },
   centeredView: {
     flex: 1,
     justifyContent: "center",
@@ -109,3 +80,5 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 });
+
+export default EULA;
