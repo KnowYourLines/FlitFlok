87bcde717bc661c87c105ae9c6c57540b6860859
Johnny <johnkcli@hotmail.com>
2024-03-01 13:56:34 +0000

    verifies app is connected to the internet

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import Home from "./components/Home.js";
+import NoInternet from "./components/NoInternet.js";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { useNetInfo } from "@react-native-community/netinfo";
 
 export default function App() {
+  const netInfo = useNetInfo();
+  if (!netInfo.isInternetReachable && netInfo.isInternetReachable !== null) {
+    return <NoInternet></NoInternet>;
+  }
   return (
     <Provider store={store}>
       <Home></Home>
