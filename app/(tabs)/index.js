3e922ba1c74d5749b59c67c26890044ee4b943a0
Johnny <johnkcli@hotmail.com>
2024-03-02 12:51:49 +0000

    move home code to home tab and initial client file

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,8 +1,32 @@
 import { StyleSheet, Text, View } from "react-native";
+import { useDispatch } from "react-redux";
+import { onAuthStateChanged, signInAnonymously } from "firebase/auth";
+import { auth } from "../../firebaseConfig.js";
+import { agreeEula } from "../../redux/eula.js";
+import EULA from "../../components/EULA.js";
 
 export default function Page() {
+  const backendUrl = process.env.EXPO_PUBLIC_BACKEND_URL;
+  const dispatch = useDispatch();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      user.getIdToken(true).then(async (token) => {
+        const response = await fetch(`${backendUrl}/eula-agreed/`, {
+          method: "GET",
+          headers: new Headers({
+            Authorization: token,
+          }),
+        });
+        const ResponseJson = await response.json();
+        dispatch(agreeEula(ResponseJson.agreed_to_eula));
+      });
+    } else {
+      signInAnonymously(auth);
+    }
+  });
   return (
     <View style={styles.container}>
+      <EULA />
       <View style={styles.main}>
         <Text style={styles.title}>Hello World</Text>
         <Text style={styles.subtitle}>This is the home page of your app.</Text>
--- app/_layout.js
+++ app/_layout.js
@@ -1,9 +1,19 @@
 import { Stack } from "expo-router/stack";
+import NoInternet from "../components/NoInternet.js";
+import { store } from "../redux/store.js";
+import { Provider } from "react-redux";
+import { useNetInfo } from "@react-native-community/netinfo";
 
 export default function AppLayout() {
+  const netInfo = useNetInfo();
+  if (!netInfo.isInternetReachable && netInfo.isInternetReachable !== null) {
+    return <NoInternet></NoInternet>;
+  }
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <Provider store={store}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </Provider>
   );
 }
