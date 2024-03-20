449e9d0f7ef598ee8100e0bf9f1ffbbb826896ce
Johnny <johnkcli@hotmail.com>
2024-03-20 03:54:27 +0000

    remove unused test tab

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -24,15 +24,6 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="test"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="home" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
         name="record"
         options={{
           title: "Record",
