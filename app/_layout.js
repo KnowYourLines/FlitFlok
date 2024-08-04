775b694d05995c3cb1bf0ee3058cb2bbca845185
Johnny <johnkcli@hotmail.com>
2024-08-04 19:23:13 +0100

    persist redux store

--- app/_layout.js
+++ app/_layout.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Stack } from "expo-router/stack";
-import { store } from "../redux/store.js";
+import { persistor, store } from "../redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function AppLayout() {
   return (
     <Provider store={store}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
+      <PersistGate loading={null} persistor={persistor}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </PersistGate>
     </Provider>
   );
 }
--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import eulaReducer from "./eula";
 import currencyReducer from "./currency";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({ eula: eulaReducer, currency: currencyReducer })
+);
+
 export const store = configureStore({
-  reducer: { eula: eulaReducer, currency: currencyReducer },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
